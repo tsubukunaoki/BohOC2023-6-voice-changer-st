76144e04680af50d3dea56a533c105a003045bdc
津布久　尚貴　tsubuku/naoki <130821076+tsubukunaoki@users.noreply.github.com>
2023-06-23 16:46:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.title("タイトルを入力")
 
 # 1つ目のオーディオファイル
 st.header("タイトルを入力")
-data1 = "audios/tanjiro.mp3"
+data1 = "audios/SnapSave.io - 「鬼滅の刃」竈門炭治郎の名言・台詞まとめ (128 kbps).mp3"
 st.audio(data1)
 
 # 2つ目のオーディオファイル
