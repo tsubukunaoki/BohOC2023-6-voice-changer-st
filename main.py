32d4d6a30b4cb3cc2b7943a022b6669d8a2fbfc3
津布久　尚貴　tsubuku/naoki <130821076+tsubukunaoki@users.noreply.github.com>
2023-06-23 17:28:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.title("タイトルを入力")
+st.title("ボイスチェンジャー")
 
 # 1つ目のオーディオファイル
-st.header("タイトルを入力")
+st.header("元の音声")
 data1 = "audios/SnapSave.io - 「鬼滅の刃」竈門炭治郎の名言・台詞まとめ (128 kbps).mp3"
 st.audio(data1)
 
 # 2つ目のオーディオファイル
-st.header("タイトルを入力")
+st.header("変換後の音声")
 data2 = "audios/tanjiro.mp3"
 st.audio(data2)
