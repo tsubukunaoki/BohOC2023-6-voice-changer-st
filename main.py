d03d4903392620fc46f3dd8fe7e04f642d898426
津布久　尚貴　tsubuku/naoki <130821076+tsubukunaoki@users.noreply.github.com>
2023-06-23 16:33:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ st.title("タイトルを入力")
 
 # 1つ目のオーディオファイル
 st.header("タイトルを入力")
-data1 = ""
-# st.audio(data1)
+data1 = "audios/tanjiro.mp3"
+st.audio(data1)
 
 # 2つ目のオーディオファイル
 st.header("タイトルを入力")
-data2 = ""
-# st.audio(data2)
+data2 = "audios/TsubukuVoice.mp3"
+st.audio(data2)
