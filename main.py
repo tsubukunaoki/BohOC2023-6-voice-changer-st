745061793e1a2a66241add0293067c6c829e2326
津布久　尚貴　tsubuku/naoki <130821076+tsubukunaoki@users.noreply.github.com>
2023-06-23 16:25:59 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("タイトルを入力")
+
+# 1つ目のオーディオファイル
+st.header("タイトルを入力")
+data1 = ""
+# st.audio(data1)
+
+# 2つ目のオーディオファイル
+st.header("タイトルを入力")
+data2 = ""
+# st.audio(data2)
