5efedfc7c4c1e6ec0e135269c178f78c711b39a2
津布久　尚貴　tsubuku/naoki <130821076+tsubukunaoki@users.noreply.github.com>
2023-06-25 08:40:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ st.title("ボイスチェンジャー")
 
 # 1つ目のオーディオファイル
 st.header("元の音声")
-data1 = "TsubukuVoice.mp3"
+data1 = "audios/TsubukuVoice.mp3"
 st.audio(data1)
 
 # 2つ目のオーディオファイル
 st.header("彼方このえの変換音声")
-data2 = "audios/tanjiro.mp3"
+data2 = "audios/tsubuku_voice_raburaibu.mp3"
+st.audio(data2)
+
+st.header("竈門炭治郎の変換voice")
+data2 = "audios/tsubuku_voice_raburaibu.mp3"
 st.audio(data2)
