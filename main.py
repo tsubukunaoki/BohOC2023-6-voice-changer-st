6511c0b93275f048ac39347c2beb0f786a08993e
津布久　尚貴　tsubuku/naoki <130821076+tsubukunaoki@users.noreply.github.com>
2023-06-25 08:36:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ st.title("ボイスチェンジャー")
 
 # 1つ目のオーディオファイル
 st.header("元の音声")
-data1 = "audios/SnapSave.io - 「鬼滅の刃」竈門炭治郎の名言・台詞まとめ (128 kbps).mp3"
+data1 = "TsubukuVoice.mp3"
 st.audio(data1)
 
 # 2つ目のオーディオファイル
-st.header("変換後の音声")
+st.header("彼方このえの変換音声")
 data2 = "audios/tanjiro.mp3"
 st.audio(data2)
